baa46737ef87951c5581046a40daae2989455b93
AntonioOliva27 <antoniolucasoliva@gmail.com>
2026-07-14 20:22:33 -0300

    ADD: declaração da função de multiplicação de um quatérnio por um escalar no arquivo header.

--- include/quaterniom.h
+++ include/quaterniom.h
@@ -11,6 +11,9 @@ typedef struct {
 // Adição de Quatérnios
 Quat quat_add(Quat q1, Quat q2);
 
+// Multiplicação por um escalar
+Quat quat_scale(Quat q1, double s);
+
 // Multiplicação de Quatérnios (Combina duas rotações em uma).
 Quat quat_mul(Quat q1, Quat q2);
 
