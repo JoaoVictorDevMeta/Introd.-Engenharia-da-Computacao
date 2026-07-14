5c3a34737912d8dea8fad707f620fd4d3186e676
AntonioOliva27 <antoniolucasoliva@gmail.com>
2026-07-14 00:05:42 -0300

    ADD: implementadas as funções de rotações (x, y, z).

---
+++ src/matrix3.c
@@ -0,0 +1,35 @@
+#include "matrix3.h"
+#include <math.h>
+
+Mat3 mat3_rotation_x(double angle){
+	double s = sin(angle);
+	double c = cos(angle);
+	
+	return (Mat3){{
+		{1, 0, 0},
+		{0, c, -s},
+		{0, s, c}
+	}};
+}
+
+Mat3 mat3_rotation_y(double angle){
+	double s = sin(angle);
+        double c = cos(angle);
+
+        return (Mat3){{
+                {c, 0, s},
+                {0, 1, 0},
+                {-s, 0, c}
+        }};
+}
+
+Mat3 mat3_rotation_z(double angle){
+	double s = sin(angle);
+        double c = cos(angle);
+
+        return (Mat3){{
+                {c, -s, 0},
+                {s, c, 0},
+                {0, 0, 1}
+        }};
+}
