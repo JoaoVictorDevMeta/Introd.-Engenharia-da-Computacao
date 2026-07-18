474e2eb6812c257d3c5f7b86ae325a6a3f59e593
AntonioOliva27 <antoniolucasoliva@gmail.com>
2026-07-18 20:48:49 -0300

    UPDATE: agora o script também executa o arquivo 'phoenix'.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -o phoenix src/*.c -Iinclude -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+gcc -o phoenix src/*.c -Iinclude -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 && ./phoenix
